f795cf32800d6fa0f6c2641498fe402ffeee59fd
Ilya Karpov <karpovila333@gmail.com>
2025-06-16 13:13:15 +0700

    update cors

--- constants/corsHeaders.ts
+++ constants/corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
     headers: {
-      'Access-Control-Allow-Origin': 'https://kkrit-admin.vercel.app',
+      'Access-Control-Allow-Origin': 'https://kkrit-admin-azml.onrender.com',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
